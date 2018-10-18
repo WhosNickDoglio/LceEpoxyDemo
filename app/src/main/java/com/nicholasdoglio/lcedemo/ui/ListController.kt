18c313653ce2c4199281a1be141d45207f9145d3
Nicholas Doglio <NicholasDoglio@Gmail.com>
2018-10-17 21:47:33 -0400

    Cleaned up ListController

--- app/src/main/java/com/nicholasdoglio/lcedemo/ui/ListController.kt
+++ app/src/main/java/com/nicholasdoglio/lcedemo/ui/ListController.kt
@@ -2,39 +2,53 @@ package com.nicholasdoglio.lcedemo.ui
 
 import com.airbnb.epoxy.TypedEpoxyController
 import com.nicholasdoglio.lcedemo.data.RequestState
-import com.nicholasdoglio.lcedemo.ui.epoxy.model.itemEmpty
-import com.nicholasdoglio.lcedemo.ui.epoxy.model.itemError
-import com.nicholasdoglio.lcedemo.ui.epoxy.model.itemListItem
-import com.nicholasdoglio.lcedemo.ui.epoxy.model.itemLoading
+import com.nicholasdoglio.lcedemo.ui.epoxy.itemEmpty
+import com.nicholasdoglio.lcedemo.ui.epoxy.itemError
+import com.nicholasdoglio.lcedemo.ui.epoxy.itemListItem
+import com.nicholasdoglio.lcedemo.ui.epoxy.itemLoading
 
 //TODO explore other Epoxy features
 class ListController(private val retry: () -> Unit) :
     TypedEpoxyController<RequestState<List<String>>>() {
 
     override fun buildModels(state: RequestState<List<String>>) = when (state) {
-        is RequestState.Loading -> {
-            itemLoading {
-                id("Loading")
-            }
+        is RequestState.Loading -> renderLoading()
+        is RequestState.Success -> renderSuccess(state.data)
+        is RequestState.Error -> renderError()
+    }
+
+    private fun renderError() {
+        itemError {
+            id(ERROR_ID)
+            onClickListener { retry() }
         }
-        is RequestState.Success -> {
-            for (item in state.data) {
+    }
+
+    private fun renderSuccess(data: List<String>) {
+        if (data.isEmpty()) {
+            itemEmpty {
+                id(EMPTY_ID)
+            }
+        } else {
+
+            data.forEach {
                 itemListItem {
-                    id(item)
-                    data(item)
+                    id(it)
+                    data(it)
                 }
             }
         }
-        is RequestState.Empty -> {
-            itemEmpty {
-                id("Empty")
-            }
+    }
+
+    private fun renderLoading() {
+        itemLoading {
+            id(LOADING_ID)
         }
-        is RequestState.Error -> {
-            itemError {
-                id("Error")
-                onClickListener { retry() }
-            }
-        }
+    }
+
+    companion object {
+        private const val ERROR_ID = "Error"
+        private const val EMPTY_ID = "Empty"
+        private const val LOADING_ID = "Loading"
     }
 }
