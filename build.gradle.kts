e6ae495b96435de0308f5da50d51f5f7d8785f10
Nicholas Doglio <NicholasDoglio@Gmail.com>
2018-12-17 19:49:35 -0500

    Updated AGP to 3.4.0-alpha08

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0-rc02")
+        classpath("com.android.tools.build:gradle:3.4.0-alpha08")
         classpath(kotlin("gradle-plugin", version = "1.3.11"))
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
@@ -13,7 +13,7 @@ buildscript {
 }
 
 plugins {
-    id("jmfayard.github.io.gradle-kotlin-dsl-libs") version "0.2.6"
+    id("de.fayard.buildSrcVersions") version "0.3.2"
 }
 
 allprojects {
