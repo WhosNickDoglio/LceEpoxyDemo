b588527296b466d808dabb7206ef618f85c3a39b
Nicholas Doglio <NicholasDoglio@Gmail.com>
2019-02-04 19:52:30 -0500

    Updated dependency versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -29,31 +29,31 @@
  *
  * YOU are responsible for updating manually the dependency version. */
 object Versions {
-    const val appcompat: String = "1.0.2" 
+    const val appcompat: String = "1.0.2"
 
-    const val constraintlayout: String = "2.0.0-alpha2" 
+    const val constraintlayout: String = "2.0.0-alpha2"
 
     const val espresso_core: String = "3.1.1"
 
     const val androidx_test_runner: String = "1.1.1"
 
-    const val com_airbnb_android: String = "3.1.0"
+    const val com_airbnb_android: String = "3.2.0"
 
-    const val com_android_tools_build_gradle: String = "3.3.0"
+    const val com_android_tools_build_gradle: String = "3.3.0" 
 
     const val lint_gradle: String = "26.3.0" 
 
-    const val de_fayard_buildsrcversions_gradle_plugin: String = "0.3.2" 
+    const val de_fayard_buildsrcversions_gradle_plugin: String = "0.3.2"
 
-    const val rxandroid: String = "2.1.0" 
+    const val rxandroid: String = "2.1.0"
 
-    const val rxjava: String = "2.2.5"
+    const val rxjava: String = "2.2.6"
 
-    const val junit: String = "4.12" 
+    const val junit: String = "4.12"
 
-    const val org_gradle_kotlin_kotlin_dsl_gradle_plugin: String = "1.1.1" 
+    const val org_gradle_kotlin_kotlin_dsl_gradle_plugin: String = "1.1.3"
 
-    const val org_jetbrains_kotlin: String = "1.3.11" 
+    const val org_jetbrains_kotlin: String = "1.3.20" 
 
     /**
      *
@@ -61,11 +61,11 @@ object Versions {
      *      ./gradle/wrapper/gradle-wrapper.properties
      */
     object Gradle {
-        const val runningVersion: String = "5.1.1"
+        const val runningVersion: String = "5.2"
 
-        const val currentVersion: String = "5.1.1"
+        const val currentVersion: String = "5.2"
 
-        const val nightlyVersion: String = "5.2-20190115022437+0000"
+        const val nightlyVersion: String = "5.3-20190205001603+0000"
 
         const val releaseCandidate: String = ""
     }
